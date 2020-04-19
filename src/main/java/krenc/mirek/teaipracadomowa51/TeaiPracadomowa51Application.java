7d7f676101b8aeb903279d54c1e59ebb5eb14363
kremir <kremir@o2.pl>
2020-04-19 13:08:32 +0200

    Final amendments

---
+++ src/main/java/krenc/mirek/teaipracadomowa51/TeaiPracadomowa51Application.java
@@ -0,0 +1,13 @@
+package krenc.mirek.teaipracadomowa51;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeaiPracadomowa51Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeaiPracadomowa51Application.class, args);
+    }
+
+}
---
+++ src/test/java/krenc/mirek/teaipracadomowa51/TeaiPracadomowa51ApplicationTests.java
@@ -0,0 +1,13 @@
+package krenc.mirek.teaipracadomowa51;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeaiPracadomowa51ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
